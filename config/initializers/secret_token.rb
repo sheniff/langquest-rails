1b0b9f05d35864ddfc256280b788365bdf5719e4
Sergio Almecija <sergio.almecija@tapjoy.com>
2013-01-20 22:20:23 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Langquest::Application.config.secret_token = '90ff5dbc2d5da8a128cc0002d855e837f70fa5099f74765c173231638d2b6e8b08f6708a132ea92e496cc3a751a9ceb4f3886bbe32d1c455c2068e0ee626a776'
